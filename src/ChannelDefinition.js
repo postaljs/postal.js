db0e59314be6692cfab517a3d5ad739c68b428fb
Jim Cowart <ashbylane@gmail.com>
2011-09-11 22:06:48 -0400

    Fixing bugs I introduced in last commit

--- src/ChannelDefinition.js
+++ src/ChannelDefinition.js
@@ -4,8 +4,8 @@ var DEFAULT_EXCHANGE = "/",
     NO_OP = function() { };
 
 var defaultConfiguration = {
-    exchange: exchange || DEFAULT_EXCHANGE,
-    topic: topic || "",
+    exchange: DEFAULT_EXCHANGE,
+    topic: "",
     callback: NO_OP,
     priority: DEFAULT_PRIORITY,
     constraints: [],
@@ -16,7 +16,7 @@ var defaultConfiguration = {
 };
 
 var ChannelDefinition = function(exchange, topic) {
-    this.configuration = _.extend(defaultConfiguration { exchange: exchange, topic: topic });
+    this.configuration = _.extend(defaultConfiguration, { exchange: exchange, topic: topic });
 } ;
 
 ChannelDefinition.prototype = {
