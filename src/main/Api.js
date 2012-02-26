60e20444e13c400276840b15b29c00b748cf6214
Jim Cowart <ashbylane@gmail.com>
2012-02-26 16:26:08 -0500

    Fixing auto-indent issues

--- src/main/Api.js
+++ src/main/Api.js
@@ -1,32 +1,32 @@
 var postal = {
-    configuration: {
-        bus: localBus,
-        resolver: bindingsResolver
-    },
+	configuration: {
+		bus: localBus,
+		resolver: bindingsResolver
+	},
 
-    channel: function(exchange, topic) {
-        var exch = arguments.length === 2 ? exchange : DEFAULT_EXCHANGE,
-            tpc  = arguments.length === 2 ? topic : exchange;
-        return new ChannelDefinition(exch, tpc);
-    },
+	channel: function(exchange, topic) {
+		var exch = arguments.length === 2 ? exchange : DEFAULT_EXCHANGE,
+			tpc  = arguments.length === 2 ? topic : exchange;
+		return new ChannelDefinition(exch, tpc);
+	},
 
-    subscribe: function(exchange, topic, callback) {
-        var exch = arguments.length === 3 ? exchange : DEFAULT_EXCHANGE,
-            tpc  = arguments.length === 3 ? topic : exchange,
-            callbk  = arguments.length === 3 ? callback : topic;
-        var channel = this.channel(exch, tpc);
-        return channel.subscribe(callbk);
-    },
+	subscribe: function(exchange, topic, callback) {
+		var exch = arguments.length === 3 ? exchange : DEFAULT_EXCHANGE,
+			tpc  = arguments.length === 3 ? topic : exchange,
+			callbk  = arguments.length === 3 ? callback : topic;
+		var channel = this.channel(exch, tpc);
+		return channel.subscribe(callbk);
+	},
 
-    publish: function(exchange, topic, payload, envelopeOptions) {
-        var parsedArgs = parsePublishArgs([].slice.call(arguments,0));
-        var channel = this.channel(parsedArgs.envelope.exchange, parsedArgs.envelope.topic);
-        channel.publish(parsedArgs.payload, parsedArgs.envelope);
-    },
+	publish: function(exchange, topic, payload, envelopeOptions) {
+		var parsedArgs = parsePublishArgs([].slice.call(arguments,0));
+		var channel = this.channel(parsedArgs.envelope.exchange, parsedArgs.envelope.topic);
+		channel.publish(parsedArgs.payload, parsedArgs.envelope);
+	},
 
-    addWireTap: function(callback) {
-        return this.configuration.bus.addWireTap(callback);
-    },
+	addWireTap: function(callback) {
+		return this.configuration.bus.addWireTap(callback);
+	},
 
 	bindExchanges: function(sources, destinations) {
 		var subscriptions = [];
--- src/main/LocalBus.js
+++ src/main/LocalBus.js
@@ -1,80 +1,80 @@
 var localBus = {
 
-    subscriptions: {},
+	subscriptions: {},
 
-    wireTaps: [],
+	wireTaps: [],
 
-    publish: function(data, envelope) {
-        this.notifyTaps(data, envelope);
+	publish: function(data, envelope) {
+		this.notifyTaps(data, envelope);
 
-        _.each(this.subscriptions[envelope.exchange], function(topic) {
-            _.each(topic, function(binding){
-                if(postal.configuration.resolver.compare(binding.topic, envelope.topic)) {
-                    if(_.all(binding.constraints, function(constraint) { return constraint(data); })) {
-                        if(typeof binding.callback === 'function') {
-                            binding.callback.apply(binding.context, [data, envelope]);
-                            binding.onHandled();
-                        }
-                    }
-                }
-            });
-        });
-    },
+		_.each(this.subscriptions[envelope.exchange], function(topic) {
+			_.each(topic, function(binding){
+				if(postal.configuration.resolver.compare(binding.topic, envelope.topic)) {
+					if(_.all(binding.constraints, function(constraint) { return constraint(data); })) {
+						if(typeof binding.callback === 'function') {
+							binding.callback.apply(binding.context, [data, envelope]);
+							binding.onHandled();
+						}
+					}
+				}
+			});
+		});
+	},
 
-    subscribe: function(subDef) {
-        var idx, found, fn;
+	subscribe: function(subDef) {
+		var idx, found, fn;
 
-        if(!this.subscriptions[subDef.exchange]) {
-            this.subscriptions[subDef.exchange] = {};
-        }
-        if(!this.subscriptions[subDef.exchange][subDef.topic]) {
-            this.subscriptions[subDef.exchange][subDef.topic] = [];
-        }
+		if(!this.subscriptions[subDef.exchange]) {
+			this.subscriptions[subDef.exchange] = {};
+		}
+		if(!this.subscriptions[subDef.exchange][subDef.topic]) {
+			this.subscriptions[subDef.exchange][subDef.topic] = [];
+		}
 
-        idx = this.subscriptions[subDef.exchange][subDef.topic].length - 1;
-        if(!_.any(this.subscriptions[subDef.exchange][subDef.topic], function(cfg) { return cfg === subDef; })) {
-            for(; idx >= 0; idx--) {
-                if(this.subscriptions[subDef.exchange][subDef.topic][idx].priority <= subDef.priority) {
-                    this.subscriptions[subDef.exchange][subDef.topic].splice(idx + 1, 0, subDef);
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) {
-                this.subscriptions[subDef.exchange][subDef.topic].unshift(subDef);
-            }
-        }
+		idx = this.subscriptions[subDef.exchange][subDef.topic].length - 1;
+		if(!_.any(this.subscriptions[subDef.exchange][subDef.topic], function(cfg) { return cfg === subDef; })) {
+			for(; idx >= 0; idx--) {
+				if(this.subscriptions[subDef.exchange][subDef.topic][idx].priority <= subDef.priority) {
+					this.subscriptions[subDef.exchange][subDef.topic].splice(idx + 1, 0, subDef);
+					found = true;
+					break;
+				}
+			}
+			if(!found) {
+				this.subscriptions[subDef.exchange][subDef.topic].unshift(subDef);
+			}
+		}
 
-        return _.bind(function() { this.unsubscribe(subDef); }, this);
-    },
+		return _.bind(function() { this.unsubscribe(subDef); }, this);
+	},
 
-    notifyTaps: function(data, envelope) {
-        _.each(this.wireTaps,function(tap) {
-            tap(data, envelope);
-        });
-    },
+	notifyTaps: function(data, envelope) {
+		_.each(this.wireTaps,function(tap) {
+			tap(data, envelope);
+		});
+	},
 
-    unsubscribe: function(config) {
-        if(this.subscriptions[config.exchange][config.topic]) {
-            var len = this.subscriptions[config.exchange][config.topic].length,
-                idx = 0;
-            for ( ; idx < len; idx++ ) {
-                if (this.subscriptions[config.exchange][config.topic][idx] === config) {
-                    this.subscriptions[config.exchange][config.topic].splice( idx, 1 );
-                    break;
-                }
-            }
-        }
-    },
+	unsubscribe: function(config) {
+		if(this.subscriptions[config.exchange][config.topic]) {
+			var len = this.subscriptions[config.exchange][config.topic].length,
+				idx = 0;
+			for ( ; idx < len; idx++ ) {
+				if (this.subscriptions[config.exchange][config.topic][idx] === config) {
+					this.subscriptions[config.exchange][config.topic].splice( idx, 1 );
+					break;
+				}
+			}
+		}
+	},
 
-    addWireTap: function(callback) {
-	    var self = this;
-	    self.wireTaps.push(callback);
-        return function() {
-            var idx = self.wireTaps.indexOf(callback);
-            if(idx !== -1) {
-	            self.wireTaps.splice(idx,1);
-            }
-        };
-    }
+	addWireTap: function(callback) {
+		var self = this;
+		self.wireTaps.push(callback);
+		return function() {
+			var idx = self.wireTaps.indexOf(callback);
+			if(idx !== -1) {
+				self.wireTaps.splice(idx,1);
+			}
+		};
+	}
 };
