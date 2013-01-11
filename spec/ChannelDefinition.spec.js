2cd58733d84297fe08a22726477aac9b02b7f533
Jim Cowart <ashbylane@gmail.com>
2013-01-11 17:02:32 -0500

    Added tests for ChannelDefinition.publish

--- spec/ChannelDefinition.spec.js
+++ spec/ChannelDefinition.spec.js
@@ -13,4 +13,35 @@ describe( "ChannelDefinition", function () {
 			expect( sub instanceof SubscriptionDefinition ).to.be.ok();
 		} );
 	} );
+	describe( "When publishing from a channel definition", function(){
+		var channel, subscription;
+		beforeEach(function(){
+			channel = postal.channel("OhHai");
+		});
+		afterEach(function(){
+			postal.utils.reset();
+			channel = undefined;
+			subscription = undefined;
+		});
+		it( "Should allow a topic only to be used", function(){
+			subscription = channel.subscribe("topic.only", function(d, e) {
+				expect(typeof d === "undefined" ).to.be(true);
+			});
+			channel.publish("topic.only");
+		});
+		it( "Should allow a topic and data argument to be used", function(){
+			subscription = channel.subscribe("topic.and.data", function(d, e) {
+				expect(d).to.be("hai");
+			});
+			channel.publish("topic.and.data", "hai");
+		});
+		it( "Should allow an envelope argument to be used", function(){
+			subscription = channel.subscribe("envelope", function(d, e) {
+				expect( e.channel).to.be("OhHai");
+				expect( e.data).to.be("hai");
+				expect( e.foo).to.be("bar");
+			});
+			channel.publish({ topic: "envelope", data: "hai", foo: "bar" });
+		});
+	});
 } );
