7c7c023952ede3bf18cadb1dbbb45c3bf2208a98
Lars Kappert <lars@webpro.nl>
2013-10-08 08:05:45 +0200

    Added indexOf to shims

--- ext/es5-shim.custom.js
+++ ext/es5-shim.custom.js
@@ -6,6 +6,41 @@ if ( !Array.isArray ) {
 	};
 }
 
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility
+
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+		'use strict';
+		if (this == null) {
+			throw new TypeError();
+		}
+		var n, k, t = Object(this),
+				len = t.length >>> 0;
+
+		if (len === 0) {
+			return -1;
+		}
+		n = 0;
+		if (arguments.length > 1) {
+			n = Number(arguments[1]);
+			if (n != n) { // shortcut for verifying if it's NaN
+				n = 0;
+			} else if (n != 0 && n != Infinity && n != -Infinity) {
+				n = (n > 0 || -1) * Math.floor(Math.abs(n));
+			}
+		}
+		if (n >= len) {
+			return -1;
+		}
+		for (k = n >= 0 ? n : Math.max(len - Math.abs(n), 0); k < len; k++) {
+			if (k in t && t[k] === searchElement) {
+				return k;
+			}
+		}
+		return -1;
+	};
+}
+
 // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Compatibility
 
 // Production steps of ECMA-262, Edition 5, 15.4.4.18
