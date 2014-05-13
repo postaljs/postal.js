8184c97ab26959fd44f2f9665d25a26d5379b96c
ifandelse <jim@ifandelse.com>
2014-05-13 15:44:58 -0400

    Removed embedded conduit. Refactored to make publish, susbcribe and unsusbcribe conduit methods

--- example/amd/js/main.js
+++ example/amd/js/main.js
@@ -1,19 +1,20 @@
-require.config( {
-	paths : {
-		underscore  : "../../../bower/underscore/underscore-min",
-		postal      : "../../../lib/postal",
-		postaldiags : "../../../bower/postal.diagnostics/lib/postal.diagnostics",
-		jquery      : "../../../bower/jquery/jquery.min"
-	},
-    shim : {
+require.config({
+    paths: {
+        underscore: "../../../bower/underscore/underscore-min",
+        postal: "../../../lib/postal",
+        postaldiags: "../../../bower/postal.diagnostics/lib/postal.diagnostics",
+        jquery: "../../../bower/jquery/jquery.min",
+        conduit: "../../../bower/conduitjs/lib/conduit.min"
+    },
+    shim: {
         underscore: {
             exports: "_"
         }
     }
-} );
+});
 
-require( [ "jquery" ], function ( $ ) {
-	$( function () {
-		require( [ "examples" ] );
-	} );
-} );
+require(["jquery"], function($) {
+    $(function() {
+        require(["examples"]);
+    });
+});
