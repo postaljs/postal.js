9a67b513ddf547e30fe46d4de39aceba4e18aa8c
Jim Cowart <jim@ifandelse.com>
2015-01-24 00:21:55 -0500

    Slight change to ChannelDefinition publish

--- src/ChannelDefinition.js
+++ src/ChannelDefinition.js
@@ -1,27 +1,33 @@
 /* global _postal, SubscriptionDefinition, _config */
 
 var ChannelDefinition = function( channelName, bus ) {
-	this.bus = bus;
-	this.channel = channelName || _config.DEFAULT_CHANNEL;
+    this.bus = bus;
+    this.channel = channelName || _config.DEFAULT_CHANNEL;
 };
 
 ChannelDefinition.prototype.subscribe = function() {
-	return this.bus.subscribe( {
-		channel: this.channel,
-		topic: ( arguments.length === 1 ? arguments[ 0 ].topic : arguments[ 0 ] ),
-		callback: ( arguments.length === 1 ? arguments[ 0 ].callback : arguments[ 1 ] )
-	} );
+    return this.bus.subscribe( {
+        channel: this.channel,
+        topic: ( arguments.length === 1 ? arguments[ 0 ].topic : arguments[ 0 ] ),
+        callback: ( arguments.length === 1 ? arguments[ 0 ].callback : arguments[ 1 ] )
+    } );
 };
 
+/*
+    publish( envelope [, callback ] );
+    publish( topic, data [, callback ] );
+*/
 ChannelDefinition.prototype.publish = function() {
-	var envelope, callback;
-	if ( arguments.length === 1 ) {
-		envelope = Object.prototype.toString.call( arguments[ 0 ] ) === "[object String]" ? { topic: arguments[ 0 ] } : arguments[ 0 ];
-		callback = arguments[ 1 ];
-	} else {
-		envelope = { topic: arguments[ 0 ], data: arguments[ 1 ] };
-		callback = arguments[ 2 ];
-	}
-	envelope.channel = this.channel;
-	this.bus.publish( envelope, callback );
+    var envelope = {};
+    var callback;
+    if ( typeof arguments[ 0 ] === "string" ) {
+        envelope.topic = arguments[ 0 ];
+        envelope.data = arguments[ 1 ];
+        callback = arguments[ 2 ];
+    } else {
+        envelope = arguments[ 0 ];
+        callback = arguments[ 1 ];
+    }
+    envelope.channel = this.channel;
+    this.bus.publish( envelope, callback );
 };
