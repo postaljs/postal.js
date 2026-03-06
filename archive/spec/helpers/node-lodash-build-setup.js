b8f48e0d552bd61e2e8c93d5e4c9e896bc5dbf34
Jim Cowart <jcowart@apogee-stealth.com>
2026-03-05 22:13:23 -0500

    Out with the old...

--- archive/spec/helpers/node-lodash-build-setup.js
+++
@@ -1,7 +0,0 @@
-/* global global */
-// Setup for running Mocha via Node
-require( "should/should" );
-
-global._ = require( "lodash" );
-
-global.postal = require( "../../lib/postal.lodash.js" );
--- archive/spec/helpers/node-setup.js
+++
@@ -1,4 +0,0 @@
-// Setup for running Mocha via Node
-require( "should/should" );
-
-global.postal = require( "../../lib/postal.js" );
